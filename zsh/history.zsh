5ed4e6396b27e83fe79a0f1f62bd98a537ff1f7d
Soeren Blom <soeren.blom@zalando.de>
2017-11-14 11:16:20 +0100

    Extracts history settings to separate file
    The settings for history are variable exports and setops. Now to be
    found 'zsh/history.zsh'.

---
+++ zsh/history.zsh
@@ -0,0 +1,36 @@
+# History
+
+HISTFILE="$HOME/.zsh_history"
+
+HISTSIZE=10000
+SAVEHIST=10000
+
+# Allow multiple terminal sessions to all append to one zsh command history
+setopt append_history
+
+# save timestamp of command and duration
+setopt extended_history
+
+# Add comamnds as they are typed, don't wait until shell exit
+setopt inc_append_history
+
+# when trimming history, lose oldest duplicates first
+setopt hist_expire_dups_first
+
+# Do not write events to history that are duplicates of previous events
+setopt hist_ignore_dups
+
+# remove command line from history when first character on the line is a space
+setopt hist_ignore_space
+
+# When searching history don't display results already cycled through twice
+setopt hist_find_no_dups
+
+# Remove extra blanks from each command line being added to history
+setopt hist_reduce_blanks
+
+# don't execute, just expand history
+setopt hist_verify
+
+# imports new commands and appends typed commands to history
+setopt share_history
--- zsh/setopt.zsh
+++ zsh/setopt.zsh
@@ -10,18 +10,6 @@ setopt pushd_ignore_dups # don't push multiple copies of the same directory onto
 # ===== Expansion and Globbing
 setopt extended_glob # treat #, ~, and ^ as part of patterns for filename generation
 
-# ===== History
-setopt append_history # Allow multiple terminal sessions to all append to one zsh command history
-setopt extended_history # save timestamp of command and duration
-setopt inc_append_history # Add comamnds as they are typed, don't wait until shell exit
-setopt hist_expire_dups_first # when trimming history, lose oldest duplicates first
-setopt hist_ignore_dups # Do not write events to history that are duplicates of previous events
-setopt hist_ignore_space # remove command line from history list when first character on the line is a space
-setopt hist_find_no_dups # When searching history don't display results already cycled through twice
-setopt hist_reduce_blanks # Remove extra blanks from each command line being added to history
-setopt hist_verify # don't execute, just expand history
-setopt share_history # imports new commands and appends typed commands to history
-
 # ===== Completion
 setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word
 setopt auto_menu # show completion menu on successive tab press. needs unsetop menu_complete to work
