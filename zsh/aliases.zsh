47f5df8b09018d2347d0dec4f70cd52f1a916610
Sören Blom <soeren.blom@zalando.de>
2018-07-16 16:22:17 +0200

    Add alias 't' for 'tmux'

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,13 @@
 
 alias zshconf="$EDITOR ~/.zshrc"
 alias dotconf="$EDITOR ~/dotfiles"
+alias t="tmux"
+alias rake="noglob rake" # necessary to make rake work inside of zsh
 
 mdp() {
 	echo mdp: Rendering $1...
 	pandoc -o /tmp/out.html -s -c ~/.gh.css $1
+	sed -i "" 's/\<body\>/\<body class=\"markdown-body\"\>/' /tmp/out.html
 	echo mdp: Finished. Opening html.
 	open /tmp/out.html
 }
@@ -13,5 +16,3 @@ NV() {
 	cd ~/Desktop/notes
 	vim `fzf --preview="cat {}" --preview-window=right:70%:wrap`
 }
-
-alias rake="noglob rake" # necessary to make rake work inside of zsh
