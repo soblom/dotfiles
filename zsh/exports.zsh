751efa56a7257db70508934ecc5a5c9ee235283c
Sören Blom <soeren.blom@infinitec.solutions>
2019-12-01 17:15:39 +0100

    Small  changes

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,3 +3,6 @@ export RBENV_ROOT=/usr/local/var/rbenv
 export JAVA_HOME="$(/usr/libexec/java_home)"
 export PATH="$PATH:$JAVA_HOME/bin/"
 export GIT_EDITOR="vim"
+
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
