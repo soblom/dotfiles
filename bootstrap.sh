ff82ee283462a7ded50fa8fc2417f90ba231a117
Soeren Blom <soeren.blom@clark.de>
2023-08-22 18:26:36 +0200

    refactor(vim): manage vim config via stow
    This is the first of several commits that will bring (neo)vim config
    management towards the GNU stow approach. vim is the test candidate for
    all other tools to follow.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,32 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 # vim: set ft=bash:
-
-
 
 # Visual Studio Code
 # ln -s "~/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code" "${HOME}/.local/bin"
+
+_stow_app() { stow -t "$HOME" "$1" ;}
+
+base() {
+  brew install stow
+}
+
+vim() {
+  _stow_app vim
+}
+
+main() {
+  if [[ -z $1 ]]; then
+    echo "Need to specifiy one or more apps to configure
+Usage: $0 <app>"
+    exit 1
+  fi
+
+  case $1 in
+    vim)
+      vim;;
+    *)
+      echo "this should never run"; exit 1;;
+  esac
+}
+
+main "$@"
