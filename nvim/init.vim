70a995ef97ef36a70f307770614b14cd44798621
Soeren Blom <soeren.blom@clark.de>
2024-03-29 18:11:44 +0100

    Vim and ZSH under new appraoch with stow

--- nvim/init.vim
+++
@@ -1,9 +0,0 @@
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vim/vimrc
-
-au TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch", timeout=250, on_visual=true}
-
-nnoremap <leader>ff <cmd>lua require'telescope.builtin'.find_files()<CR>
-nnoremap <leader>b <cmd>lua require'telescope.builtin'.buffers()<CR>
-nnoremap <leader>rg <cmd>lua require'telescope.builtin'.live_grep()<CR>
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,16 +5,24 @@ alias grdl="./gradlew"
 alias iDrive="cd ~/Library/Mobile\ Documents/com\~apple\~CloudDocs"
 alias ls="ls -lh"
 alias vim='nvim'
+alias aspell='TERM=ansi aspell'
 
 mdp() {
   if [[ -z $1 ]]; then echo "error: please specify an input file!"; exit 1; fi
-	echo "mdp: Rendering $1.."
-  local tempfile
-  tempfile="$(mktemp -t mdp_out_)"
-	pandoc -o "${tempfile}" -t html --metadata title="$1" -s -c ~/.gh.css "$1"
-	sed -i "" 's/\<body\>/\<body class=\"markdown-body\"\>/' "$tempfile"
-	echo mdp: Finished. Opening HTML.
-  mv "$tempfile" "${tempfile}.html"
-	open "${tempfile}.html"
+  echo "mdp: Rendering $1.."
+  local outfile="${1%.*}.html"
+  local themedir=$(realpath ~/.local/share/gh-theme)
+  pandoc\
+    -o "${outfile}"\
+    -t html\
+    -s --resource-path=$themedir\
+    -H inline-style.html\
+    -B begin-insert.html\
+    -A end-insert.html\
+    -c $themedir/gh.css\
+    --embed-resources\
+    -- $1
+  echo mdp: Finished. Opening HTML.
+  open $outfile
 }
 
